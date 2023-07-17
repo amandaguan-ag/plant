e5af8da90e2674a8ce7e98dea8f3cf5df52b479a
Amanda Guan <ag.amandaguan@gmail.com>
2023-07-17 16:46:09 -0700

    sucessfully bundle js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
 import "bootstrap";
 import "./css/styles.css";
-import { attachEventListeners } from "./js/ui.js";
+import Plant from "./js/plant.js";
+
+export function attachEventListeners() {
+  let plant = new Plant();
+
+  document.getElementById("hydrate").addEventListener("click", function () {
+    plant.hydrate();
+    document.getElementById("water").textContent = `Water: ${plant.water}`;
+  });
+
+  document.getElementById("feed").addEventListener("click", function () {
+    plant.feed();
+    document.getElementById("soil").textContent = `Soil: ${plant.soil}`;
+  });
+
+  document.getElementById("giveLight").addEventListener("click", function () {
+    plant.giveLight();
+    document.getElementById("light").textContent = `Light: ${plant.light}`;
+  });
+}
 
 document.addEventListener("DOMContentLoaded", function () {
   attachEventListeners();
